0aa1d84cd16662f338d3b0fe850272a01c5875b7
KobelevDmitrii <67497198+KobelevDmitrii@users.noreply.github.com>
2021-12-23 21:01:22 +0300

    add POST

--- frontend/src/Pages/login.js
+++ frontend/src/Pages/login.js
@@ -3,17 +3,38 @@ import '../index.css';
 import { Link, Outlet } from "react-router-dom";
 
 function login() {
+
+    let email = React.createRef();
+    let password = React.createRef();
+
+    function show (event){
+        let user = {
+            email: email.current.value,
+            password: password.current.value
+        };
+    }
+
+    let response = fetch('http://app:5000/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(show.user)
+    });
+    let result = response.json();
+    console.log(result.message);
+
     return (
         <div className="login">
             <div className="wrapper">
                 <div className="cart">
                     <div className="flexColum">
                         <div className="inputBox">
-                            <input type="email" placeholder="E-mail" name="E-mail" required className="input"/>
+                            <input type="email" placeholder="E-mail" name="E-mail" value="" required className="input" onInput={show} ref={email}/>
                             <hr />
                         </div>
                         <div className="inputBox">
-                            <input type="password" placeholder="Пароль" name="password" required className="input"/>
+                            <input type="password" placeholder="Пароль" name="password" value="" required className="input" onInput={show} ref={password}/>
                             <hr />
                         </div>
                         <Link to="/" className="btnCart">Войти</Link>
--- frontend/src/Pages/reg.js
+++ frontend/src/Pages/reg.js
@@ -3,25 +3,50 @@ import '../index.css';
 import { Link, Outlet } from "react-router-dom";
 
 function Reg() {
+
+    let firstname = React.createRef();
+    let lastname = React.createRef();
+    let email = React.createRef();
+    let password = React.createRef();
+
+    function show (event){
+        let user = {
+            firstname: firstname.current.value,
+            lastname: lastname.current.value,
+            email: email.current.value,
+            password: password.current.value
+        };
+    }
+
+    let response = fetch('http://app:5000/users/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(show.user)
+    });
+    let result = response.json();
+    console.log(result.message);
+
     return (
-        <div className="login">
+        <div className="login"> 
             <div className="wrapper">
                 <div className="cart">
                     <div className="flexColum">
                         <div className="inputBox">
-                            <input type="name" placeholder="Имя" name="E-mail" required className="input"/>
+                            <input type="firstname" placeholder="Имя" name="E-mail" required className="input" onInput={show} ref={firstname}/>
                             <hr />
                         </div>
                         <div className="inputBox">
-                            <input type="name" placeholder="Фамилия" name="password" required className="input"/>
+                            <input type="lastname" placeholder="Фамилия" name="password" required className="input" onInput={show} ref={lastname}/>
                             <hr />
                         </div>
                         <div className="inputBox">
-                            <input type="email" placeholder="E-mail" name="E-mail" required className="input"/>
+                            <input type="email" placeholder="E-mail" name="E-mail" required className="input" onInput={show} ref={email}/>
                             <hr />
                         </div>
                         <div className="inputBox">
-                            <input type="password" placeholder="Пароль" name="E-mail" required className="input"/>
+                            <input type="password" placeholder="Пароль" name="E-mail" required className="input"  onInput={show} ref={password}/>
                             <hr />
                         </div>
                         <Link to="/" className="btnCart textBtn">Зарегестрироваться</Link>
